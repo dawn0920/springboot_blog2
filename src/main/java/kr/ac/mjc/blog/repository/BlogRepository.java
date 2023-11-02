abd8cc37a0036dafaa6f30a59a2153c643ca3721
backgahee <backpowp0920@naver.com>
2023-11-02 15:33:44 +0900

    first commit

---
+++ src/main/java/kr/ac/mjc/blog/repository/BlogRepository.java
@@ -0,0 +1,9 @@
+package kr.ac.mjc.blog.repository;
+
+import kr.ac.mjc.blog.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article, Long> {
+
+
+}
